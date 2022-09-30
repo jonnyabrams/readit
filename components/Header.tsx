54782a9fc312589185fa8d4175358b9ff86a1967
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-30 17:09:16 +0100

    feat: remaining Header icons with custom className in globals

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,20 @@
 import Image from "next/image";
 
-import { HomeIcon, ChevronDownIcon, SearchIcon } from "@heroicons/react/solid";
-import { StarIcon } from "@heroicons/react/outline";
+import {
+  HomeIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  MenuIcon,
+} from "@heroicons/react/solid";
+import {
+  BellIcon,
+  ChatIcon,
+  GlobeIcon,
+  PlusIcon,
+  SparklesIcon,
+  SpeakerphoneIcon,
+  VideoCameraIcon,
+} from "@heroicons/react/outline";
 
 const Header = () => {
   return (
@@ -18,9 +31,24 @@ const Header = () => {
 
       <form className="flex items-center flex-1 px-3 py-1 space-x-2 bg-gray-100 border border-gray-200 rounded-sm">
         <SearchIcon className="w-6 h-6 text-gray-400" />
-        <input className="flex-1 bg-transparent outline-none" type="text" placeholder="Search Readit" />
+        <input
+          className="flex-1 bg-transparent outline-none"
+          type="text"
+          placeholder="Search Readit"
+        />
         <button type="submit" hidden />
       </form>
+
+      <div className="flex">
+        <SparklesIcon className="icon" />
+        <GlobeIcon className="icon" />
+        <VideoCameraIcon className="icon" />
+        <hr className="h-10 border border-gray-100" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <PlusIcon className="icon" />
+        <SpeakerphoneIcon className="icon" />
+      </div>
     </div>
   );
 };
