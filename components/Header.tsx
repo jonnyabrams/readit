550ff4b8fa9be13a9d233e5fa22c179bc0cdf898
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-30 17:00:07 +0100

    feat: search bar in Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import { HomeIcon, ChevronDownIcon } from "@heroicons/react/solid";
+import { HomeIcon, ChevronDownIcon, SearchIcon } from "@heroicons/react/solid";
 import { StarIcon } from "@heroicons/react/outline";
 
 const Header = () => {
@@ -15,6 +15,12 @@ const Header = () => {
         <p className="flex-1 hidden ml-2 lg:inline">Home</p>
         <ChevronDownIcon className="w-5 h-5" />
       </div>
+
+      <form className="flex items-center flex-1 px-3 py-1 space-x-2 bg-gray-100 border border-gray-200 rounded-sm">
+        <SearchIcon className="w-6 h-6 text-gray-400" />
+        <input className="flex-1 bg-transparent outline-none" type="text" placeholder="Search Readit" />
+        <button type="submit" hidden />
+      </form>
     </div>
   );
 };
