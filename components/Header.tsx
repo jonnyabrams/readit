6838c249bcc34d9e2710feecec9037c04cb780be
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-30 16:50:37 +0100

    feat: add icons to Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,22 @@
-import Image from "next/image"
+import Image from "next/image";
+
+import { HomeIcon, ChevronDownIcon } from "@heroicons/react/solid";
+import { StarIcon } from "@heroicons/react/outline";
 
 const Header = () => {
   return (
-    <div>
-      <div className="relative w-20 h-10">
+    <div className="flex px-4 py-2 bg-white shadow-sm">
+      <div className="relative flex-shrink-0 w-20 h-10 cursor-pointer">
         <Image objectFit="contain" src="/logo.jpeg" layout="fill" />
       </div>
-    </div>
-  )
-}
 
-export default Header
+      <div className="flex items-center mx-7">
+        <HomeIcon className="w-5 h-5" />
+        <p>Home</p>
+        <ChevronDownIcon className="w-5 h-5" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
