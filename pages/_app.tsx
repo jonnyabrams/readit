9b61b90546345522cbc6b2b7a7960a2ab19e946f
jonnyabrams <jonnyabrams@hotmail.com>
2022-10-01 10:16:15 +0100

    feat: bring apollo client into _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
+import { ApolloProvider } from "@apollo/client";
 
+import client from '../apollo-client'
 import "../styles/globals.css";
 import Header from "../components/Header";
 
@@ -12,13 +14,15 @@ const MyApp = ({
   session: Session;
 }>) => {
   return (
-    <SessionProvider session={pageProps.session}>
-      <div className="h-screen overflow-y-scroll bg-slate-200">
-        <Header />
-      <Component {...pageProps} />
-      </div>
-    </SessionProvider>
+    <ApolloProvider client={client}>
+      <SessionProvider session={pageProps.session}>
+        <div className="h-screen overflow-y-scroll bg-slate-200">
+          <Header />
+          <Component {...pageProps} />
+        </div>
+      </SessionProvider>
+    </ApolloProvider>
   );
-}
+};
 
 export default MyApp;
