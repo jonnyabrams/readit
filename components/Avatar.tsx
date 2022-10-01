631da343940534d9c6e33dd33dcd835511be2237
jonnyabrams <jonnyabrams@hotmail.com>
2022-10-01 17:35:23 +0100

    feat: types for Avatar seed/optional large styles

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,15 +1,24 @@
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 
-const Avatar = () => {
+type Props = {
+  seed?: string;
+  large?: boolean;
+};
+
+const Avatar = ({ seed, large }: Props) => {
   const { data: session } = useSession();
 
   return (
-    <div className="relative w-10 h-10 bg-white border-gray-300 rounded-full">
+    <div
+      className={`relative w-10 h-10 bg-white border-gray-300 rounded-full ${
+        large && "h-20 w-20"
+      }`}
+    >
       <Image
         layout="fill"
         src={`https://avatars.dicebear.com/api/open-peeps/${
-          session?.user?.name || "placeholder"
+          seed || session?.user?.name || "placeholder"
         }.svg`}
         alt=""
       />
