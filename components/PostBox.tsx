b7852a8fc47e918863c89500665003e54e19beac
jonnyabrams <jonnyabrams@hotmail.com>
2022-10-01 17:42:32 +0100

    feat: PostBox icons

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,4 +1,6 @@
 import { useSession } from "next-auth/react";
+import { LinkIcon, PhotographIcon } from "@heroicons/react/outline";
+
 import Avatar from "./Avatar";
 
 const PostBox = () => {
@@ -16,6 +18,9 @@ const PostBox = () => {
             session ? "Create a post by entering a title!" : "Sign in to post"
           }
         />
+
+        <PhotographIcon className={`h-6 text-gray-300 cursor-pointer`} />
+        <LinkIcon className="h-6 text-gray-300" />
       </div>
     </form>
   );
