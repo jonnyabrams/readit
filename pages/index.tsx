958492ab9d6fff2cc517238cf790ac48773a46b1
jonnyabrams <jonnyabrams@hotmail.com>
2022-10-03 09:40:28 +0100

    styles: constrain parameters of app in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 
-import Header from "../components/Header";
 import PostBox from "../components/PostBox";
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="max-w-5xl mx-auto my-7">
       <Head>
         <title>Readit</title>
         <link rel="icon" href="/favicon.ico" />
