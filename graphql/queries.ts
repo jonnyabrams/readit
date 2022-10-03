3c48c8dba066f89b97bd876ca2cf5d6ab73afb4b
jonnyabrams <jonnyabrams@hotmail.com>
2022-10-03 16:17:52 +0100

    feat: materializer to bring votes & comments into posts

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -1,5 +1,20 @@
 import { gql } from "@apollo/client";
 
+export const GET_ALL_POSTS = gql`
+  query MyQuery {
+    getPostList {
+      body
+      created_at
+      id
+      image
+      title
+      subreadit_id
+      username
+    }
+  }
+
+`
+
 export const GET_SUBREADIT_BY_TOPIC = gql`
   query MyQuery($topic: String!) {
     getSubreaditListByTopic(topic: $topic) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Feed from "../components/Feed";
 
 import PostBox from "../components/PostBox";
 
@@ -13,7 +14,9 @@ const Home: NextPage = () => {
 
       <PostBox />
 
-      <div className="flex"></div>
+      <div className="flex">
+        <Feed />
+      </div>
     </div>
   );
 };
