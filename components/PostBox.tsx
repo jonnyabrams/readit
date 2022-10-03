1bd73b813b8403ecd4adaf77e94c9c5eece04de3
jonnyabrams <jonnyabrams@hotmail.com>
2022-10-03 08:54:56 +0100

    feat: text in PostBox makes other inputs appear

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,16 +1,32 @@
 import { useSession } from "next-auth/react";
 import { LinkIcon, PhotographIcon } from "@heroicons/react/outline";
+import { useForm } from "react-hook-form";
 
 import Avatar from "./Avatar";
 
+type FormData = {
+  postTitle: string;
+  postBody: string;
+  postImage: string;
+  subreadit: string;
+};
+
 const PostBox = () => {
   const { data: session } = useSession();
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>();
 
   return (
     <form className="sticky z-50 p-2 bg-white border border-gray-300 rounded-md top-16">
       <div className="flex items-center space-x-3">
         <Avatar />
         <input
+          {...register("postTitle", { required: true })}
           disabled={!session}
           className="flex-1 p-2 pl-5 rounded-md outline-none bg-gray-50"
           type="text"
@@ -22,6 +38,30 @@ const PostBox = () => {
         <PhotographIcon className={`h-6 text-gray-300 cursor-pointer`} />
         <LinkIcon className="h-6 text-gray-300" />
       </div>
+
+      {!!watch("postTitle") && (
+        <div className="flex flex-col py-2">
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Body:</p>
+            <input
+              className="flex-1 p-2 m-2 outline-none bg-blue-50"
+              {...register("postBody")}
+              type="text"
+              placeholder="Text (optional)"
+            />
+          </div>
+          
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Subreadit:</p>
+            <input
+              className="flex-1 p-2 m-2 outline-none bg-blue-50"
+              {...register("subreadit")}
+              type="text"
+              placeholder="eg. Everton FC"
+            />
+          </div>
+        </div>
+      )}
     </form>
   );
 };
