ccda8ac197084d2d21d2cd5e8fae120ff01be62d
jonnyabrams <jonnyabrams@hotmail.com>
2022-10-12 13:57:02 +0100

    feat: subreadit displays only its own topic's posts

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 A Reddit clone using TypeScript, NextJS, GraphQL, Supabase, Tailwind CSS and user avatars from the DiceBear API
 
-3:15:15
+3:31:18
--- components/Feed.tsx
+++ components/Feed.tsx
@@ -8,9 +8,15 @@ type Props = {
 };
 
 const Feed = ({ topic }: Props) => {
-  const { data, error } = !topic ? useQuery(GET_ALL_POSTS) : useQuery(GET_ALL_POSTS_BY_TOPIC);
+  const { data, error } = !topic
+    ? useQuery(GET_ALL_POSTS)
+    : useQuery(GET_ALL_POSTS_BY_TOPIC, {
+        variables: {
+          topic: topic,
+        },
+      });
 
-  const posts: Post[] = data?.getPostList;
+  const posts: Post[] = !topic ? data?.getPostList : data?.getPostListByTopic;
 
   return (
     <div className="mt-5 space-y-4">
